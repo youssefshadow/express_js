1b2a17a5eb4afc71901b3d19748c62c021d0f2c2
unknown <youssef20ben@gmail.com>
2025-02-12 08:56:51 +0100

    add routes

--- app/backend/routes/goalRoutes.js
+++ app/backend/routes/goalRoutes.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const router = express.Router();
 router.get("/", (req, res) => {
-  res.json({ message: "Get goals" });
+  res.json({ message: "Get goals " });
+});
+
+router.post("/", (req, res) => {
+  res.json({ message: "Add goal " });
+});
+
+router.put("/:id", (req, res) => {
+  res.json({ message: `Update goal ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.json({ message: `Delete goal ${req.params.id}` });
 });
 
 module.exports = router;
